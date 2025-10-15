a3b7b0d1aaac11e2e58134fe02d6e3dc57dad003
Mauricio Medina <mauricio.hernan.medina@gmail.com>
2025-10-15 20:06:26 -0300

    agrega canva con turnos disponibles

--- src/main/java/com/tallerwebi/presentacion/ControladorInicio.java
+++ src/main/java/com/tallerwebi/presentacion/ControladorInicio.java
@@ -1,12 +1,28 @@
 package com.tallerwebi.presentacion;
 
+import com.tallerwebi.dominio.Cliente;
+import com.tallerwebi.dominio.ServicioCliente;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+
 @Controller
 public class ControladorInicio {
 
+    private final ServicioCliente servicioCliente;
+
+    @Autowired
+    public ControladorInicio(ServicioCliente servicioCliente) {
+        this.servicioCliente = servicioCliente;
+    }
+
     @GetMapping("/inicio")
-    public String inicio() {
+    public String inicio(Model modelo) {
+        Cliente clienteActual = servicioCliente.buscarClientePorId(101);
+        modelo.addAttribute("turnos", new ArrayList<>(clienteActual.getTurnos()));
         return "inicio"; // Thymeleaf busca /WEB-INF/views/thymeleaf/inicio.html
     }
 }
