8a7335cc1e871946e0b3d1a5a62c2d5e6736921a
Diego Contreras <166648552+DieEmaContreras@users.noreply.github.com>
2025-11-06 14:38:16 -0300

    Ultimos cambios

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,44 @@
 INSERT INTO Usuario(id, email, password, rol, activo) VALUES(null, 'test@unlam.edu.ar', 'test', 'ADMIN', true);
 
+INSERT INTO Recomendacion (titulo, descripcion, tipo, etapa, sexo, link)
+
+VALUES ('Felicidades por tu cachorro!', 'Asegurate que tu perrito conozca su ambiente, donde tiene que hacer sus necesidades y asegurate que tenga agua a mano. No le dejes toda la comida junta que se la puede llegar a comer toda junta',
+        'Perro', 'Cachorro', 'Macho', 'https://articulo.mercadolibre.com.ar/MLA-854035124-kong-puppy-large-juguete-perros-cachorros-_JM');
+
+INSERT INTO Recomendacion (titulo, descripcion, tipo, etapa, sexo, link)
+VALUES ('Vacunación y Desparasitación', 'Los primeros meses son claves. Sigue el calendario de vacunas y desparasita regularmente.',
+        'Perro', 'Cachorro', 'Ambos', 'https://www.lamatanza.gov.ar/saludpublica/centro-zoonosis');
+
+
+ INSERT INTO Recomendacion (titulo, descripcion, tipo, etapa, sexo, link)
+VALUES ('Alimento Sieger', 'Alimento específico para gatitos hasta 1 año. Ayuda al desarrollo de huesos y vista.',
+       'Gato', 'Cachorro', 'Ambos', 'https://sieger.com.ar/producto/sieger-katze-kitten/'); --
+
+INSERT INTO Recomendacion (titulo, descripcion, tipo, etapa, sexo, link)
+VALUES ('Ejercicio y Paseos', 'Mantén una rutina de ejercicio para prevenir el sobrepeso y problemas de conducta.',
+        'Perro', 'Adulto', 'Ambos', 'https://www.mercadolibre.com.ar/correa-retractil-doble-para-2-perros-linterna-y-porta-bolsa-color-negro/p/MLA61388099');
+
+
+INSERT INTO Recomendacion (titulo, descripcion, tipo, etapa, sexo, link)
+VALUES ('Control de Peso', 'Si tu perra está esterilizada, es vital controlar las porciones para evitar el aumento de peso.',
+        'Perro', 'Adulto', 'Hembra', 'https://www.mercadolibre.com.ar/alimento-cinovita-base-bajo-tenor-graso-pollo-y-carne/up/MLAU232184904');
+
+
+INSERT INTO Recomendacion (titulo, descripcion, tipo, etapa, sexo, link)
+VALUES ('Cuidado Urinario', 'Los gatos machos son propensos a problemas urinarios. Ofrece fuentes de agua y alimento bajo en magnesio.',
+        'Gato', 'Adulto', 'Macho', 'https://www.tienda.vitalcrops.com.ar/alimento-catpro-premium-castradosindoor-para-gato-adulto-x-75kg/p/MLA20882150?');
+
+
+INSERT INTO Recomendacion (titulo, descripcion, tipo, etapa, sexo, link)
+VALUES ('Juego Diario', 'Asegúrate de jugar al menos 15 minutos al día con tu gato. Es esencial para su salud mental.',
+        'Gato', 'Adulto', 'Ambos', 'https://www.mercadolibre.com.ar/gimnasio-rascador-para-gatos-juguete-bajo-dakota-felpa-sisal/p/MLA45654436');
+
+
+INSERT INTO Recomendacion (titulo, descripcion, tipo, etapa, sexo, link)
+VALUES ('Suplemento Articular', 'Considera suplementos de glucosamina y condroitina para mejorar la salud de las articulaciones.',
+        'Perro', 'Senior', 'Ambos', 'https://www.farmaciaveterinaria.com.ar/artri-vet');
+
+INSERT INTO Recomendacion (titulo, descripcion, tipo, etapa, sexo, link)
+VALUES ('Monitoreo Renal', 'En gatos mayores, es crucial realizar análisis de sangre anuales para detectar problemas renales a tiempo.',
+        'Gato', 'Senior', 'Ambos', 'https://www.mercadolibre.com.ar/condroprotector-sosten-cg-perrosgatos-x-80-comprimidos/p/MLA53751157');
+
